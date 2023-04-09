d5f8723f99748416d59e017ab1f00d4631d1167a
George Papadopoulos <george.719pap@gmail.com>
2023-04-09 21:38:04 +0300

    Draft of ArrayQueue
    Signed-off-by: George Papadopoulos <george.719pap@gmail.com>

--- src/main/kotlin/ArrayQueue.kt
+++ src/main/kotlin/ArrayQueue.kt
@@ -6,63 +6,93 @@ import kotlinx.atomicfu.update
 import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withPermit
 import java.util.concurrent.atomic.AtomicReference
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
 
 class ArrayQueue(
     initialValue: QueueJob? = null,
     /**
      * Sets array's maximum capacity.
      */
-    sizeOfArray: Int = 10,
+    private val capacity: Int = 10,
     /**
      * Sets the maximum available workers to process jobs concurrently.
      */
     private val numberOfWorkers: Int = 3,
 ) {
     // Use AtomicReference instead of AtomicRef, as workaround for https://github.com/Kotlin/kotlinx-atomicfu/issues/293
-    private val array: Array<AtomicReference<QueueJob?>> = Array(sizeOfArray) { AtomicReference(null) }
+    private val array: Array<AtomicReference<QueueJob?>> = Array(capacity) { AtomicReference(null) }
     private val _size = atomic(0)
 
-    // index for head and tail position.
+    // indexes for head and tail position.
     private val head = atomic(0)
     private val tail = atomic(0)
 
-    //TODO: add KDoc
+    /**
+     * Moves forward the index (head or tail), by updating it based on "(current_position + 1).mod(capacity)" formula.
+     * This allows us to treat the array as a closed circle, thus making it unnecessary to ever move items stores in array.
+     */
     @Suppress("NOTHING_TO_INLINE")
     private inline fun AtomicInt.moveIndexForward() {
-        update { (it + 1).mod(array.size) }
+        update { (it + 1).mod(capacity) }
     }
 
-    private fun moveHeadForwardAndDecrSize() {
+    private fun moveHeadForwardAndDecSize() {
         head.moveIndexForward()
         _size.decrementAndGet()
     }
 
-    private fun moveTailForwardAndIncrSize() {
+    private fun moveTailForwardAndIncSize() {
         tail.moveIndexForward()
         _size.incrementAndGet()
     }
 
-    //
-    private val queueAvailableForDequeue = Semaphore(1)
+    private fun tryAddLast(value: QueueJob): Boolean {
+        if (!array[tail.value].compareAndSet(null, value)) return false
+        moveTailForwardAndIncSize()
+        return true
+    }
 
-    // Represent workers as semaphores to allow suspension when there is no worker available, instead of looping
-    // continuously for free worker.
-    private val workers = Semaphore(numberOfWorkers)
-
-    private val hasCapacity: Boolean get() = _size.value < array.size
-    private val isFull: Boolean get() = _size.value == array.size
-
-    init {
-        require(numberOfWorkers > 0) { "number of workers cannot be 0 or negative, but got:$numberOfWorkers" }
-        if (initialValue != null) {
-            array[0] = AtomicReference(initialValue)
-            _size.incrementAndGet()
+    private fun removeFirstOrNull(): QueueJob? {
+        while (true) {
+            val job = array[head.value].get() ?: return null
+            if (!array[head.value].compareAndSet(job, null)) continue
+            moveHeadForwardAndDecSize()
+            return job
         }
     }
 
+    /*
+     * This semaphore represents whether there is any item in queue for retrieval. Its main purpose is to help suspend
+     * consumers who try to dequeue from queue when is empty. Consumers are suspended until next insertion.
+     *
+     * 0 -> Empty queue
+     * 1 -> Not empty queue
+     *
+     *                         States
+     *
+     *  queue is empty     |    insertion of at least one item   |  insertion of items    |    queue is empty
+     *  | ---- 0 ---- |   -->   | ---- 1 ---- |                 -->  | ---- 1 ---- |     -->   | ---- 0 ---- |
+     */
+    private val queueAvailableForDequeue = Semaphore(
+        1,
+        // Queue's initial state is empty,
+        1
+    )
+
+    // Represent workers as semaphores to allow suspension when there is not a worker available, instead of looping
+    // continuously for a free worker.
+    private val workers = Semaphore(numberOfWorkers)
+
+    private val hasCapacity: Boolean get() = _size.value < capacity
+    private val isFull: Boolean get() = _size.value == capacity
+
+    init {
+        require(numberOfWorkers > 0) { "number of workers cannot be 0 or negative, but got:$numberOfWorkers" }
+        require(capacity > 0) { "capacity of array cannot be 0 or negative, but got:$capacity" }
+        if (initialValue != null) {
+            tryAddLast(initialValue)
+            queueAvailableForDequeue.release() // release sem, since queue is not empty.
+        }
+    }
 
     val size: Int get() = _size.value
     val isEmpty: Boolean get() = size == 0
@@ -73,8 +103,7 @@ class ArrayQueue(
         workers.withPermit {
             while (true) {
                 if (isFull) return false
-                if (array[tail.value].compareAndSet(null, value)) {
-                    moveTailForwardAndIncrSize()
+                if (tryAddLast(value)) {
                     if (queueAvailableForDequeue.availablePermits == 0) {
                         // This needed in order to allow consumers suspend when they try to dequeue from queue when is empty.
                         queueAvailableForDequeue.release() // signal waiters that queue is ready for dequeue.
@@ -85,14 +114,13 @@ class ArrayQueue(
         }
     }
 
-    // Returns value or Failure if queue is empty.
-    // Suspends when there is not an available worker.
+    // Returns value or suspends if queue is empty.
+    // Also, suspends when there is not an available worker.
     suspend fun dequeue(): QueueResult {
         workers.withPermit {
             while (true) { // fast-path, queue is not empty and item is ready for retrieval.
-                if (isEmpty) return@withPermit // move to slow-path
-                val job = array[head.value].get() ?: continue // loop until there is an available head to remove.
-                moveHeadForwardAndDecrSize()
+                if (isEmpty) return@withPermit // go-to slow-path
+                val job = removeFirstOrNull() ?: continue // loop until there is an available head to remove.
                 // if queue is empty cause of this `dequeue` operation, then acquire permit (signal queue is empty), if
                 // there is one.
                 if (isEmpty) queueAvailableForDequeue.tryAcquire()
@@ -104,25 +132,11 @@ class ArrayQueue(
             queueAvailableForDequeue.acquire() // suspend until queue has an item ready for retrieval.
             workers.withPermit {
                 if (isEmpty) return@withPermit // continue, suspend again until there is an available item.
-                val job = array[head.value].get()
+                val job = removeFirstOrNull()
                     ?: return@withPermit // continue, suspend again until there is an available item.
-                moveHeadForwardAndDecrSize()
                 if (hasCapacity) queueAvailableForDequeue.release() // release sem only if this item was not the last one.
                 return Success(job.invoke(WorkerScope).await())
             }
-        }
-    }
-
-    @OptIn(ExperimentalContracts::class)
-    private suspend inline fun <T> Semaphore.suspendUntilItemInsertion(action: () -> T): T {
-        contract {
-            callsInPlace(action, InvocationKind.EXACTLY_ONCE)
-        }
-        acquire() // suspend until an item is inserted
-        try {
-            return action()
-        } finally {
-            if (!isEmpty) release() // release sem, only if queue is not empty.
         }
     }
 }
--- src/test/kotlin/ArrayQueueTest.kt
+++ src/test/kotlin/ArrayQueueTest.kt
@@ -1,5 +1,6 @@
 package asynqueueproblem.test
 
+import asynqueueproblem.Success
 import asynqueueproblem.emptyArrayQueue
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
@@ -12,9 +13,13 @@ class ArrayQueueTest {
     @Test
     fun basicTest(): Unit = runBlocking {
         val queue = emptyArrayQueue()
+        assert(queue.isEmpty)
         for (i in 1..10) {
             queue.tryEnqueue { async { i.also { println(it) } } }
         }
+        assert(!queue.tryEnqueue {
+            async { 10 }
+        })
         println("queue size:${queue.size}")
         val jobs = buildList {
             for (item in 0 until queue.size) {
@@ -22,10 +27,15 @@ class ArrayQueueTest {
             }
         }
         jobs.forEach { it.join() }
+        println("queue size:${queue.size}")
         assert(queue.isEmpty)
+        assert(queue.tryEnqueue {
+            async { 10 }
+        })
+        assert((queue.dequeue() as Success).value == 10)
     }
 
-    @Test
+    //@Test
     fun shouldSuspendWhenDequeuingEmptyQueue(): Unit = runTest {
         val queue = emptyArrayQueue()
         queue.dequeue()
